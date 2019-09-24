1b7be8eb7e3e0e41876c5486668ad976af5e3c36
Dana Cronin <cronin43224235@gmail.com>
2019-09-24 18:24:53 -0400

    adding variable pastGenres and pseudocoding random number generator before potential loop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 var genres =["rock", "instromental", "jazz"] //....... this will become a potential matrix when adding music?
 var wins = 0;
 var guessesNumber = 17;
+var pastGenres = []
 
 // "click any key to start"
 // startClick(){
@@ -11,3 +12,7 @@ var guessesNumber = 17;
 
 //var guess = string.fromCharCode(evemt.keycode).toUpperCase();
 //to capture guess into loop to compare and later array for word or incorrect guesses
+
+// word chosing from array 
+//var currentWord = genres[Math.floor(Math.random*genres.length)];
+//loop to ensure not already used
