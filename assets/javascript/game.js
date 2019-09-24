6da110a6e85f82dd2657d5024f4096df48e077e4
Dana Cronin <cronin43224235@gmail.com>
2019-09-24 18:37:10 -0400

    plotting further functions or loops / not propperly arranged yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,28 @@
-var genres =["rock", "instromental", "jazz"] //....... this will become a potential matrix when adding music?
+functionMain(){
+var genres =["rock", "instromental", "jazz"] //....... this will become a potential matrix when adding music and images?
 var wins = 0;
 var guessesNumber = 17;
 var pastGenres = []
-
+}
+//functionGame(){ to play game with exit loop being all rounds have been played
+//mayby use matrix loop here for images and music
 // "click any key to start"
 // startClick(){
 //     document.onkeyup(function) {
-
 //     }
 // }
+//} loop 1
 
 //var guess = string.fromCharCode(evemt.keycode).toUpperCase();
 //to capture guess into loop to compare and later array for word or incorrect guesses
 
+
+functionWins(){
+    
+} loop 2
+
+functionWord(){
 // word chosing from array 
 //var currentWord = genres[Math.floor(Math.random*genres.length)];
 //loop to ensure not already used
+} loop 3
