d9d465a8ef382737d289014c2b72aecd2290164f
Dana Cronin <cronin43224235@gmail.com>
2019-09-24 17:21:46 -0400

    creating first 3 variables and commenting idea to start-click

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var words =["rock", "instromental", "jazz"] //.......
+var wins = 0;
+var guessesNumber = 17;
+
+// "click any key to start"
+// startClick(){
+//     document.onkeyup(function) {
+
+//     }
+// }
+
