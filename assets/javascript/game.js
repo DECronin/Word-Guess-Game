51db53235e53beb1e37fc0693d21a8659c53e25f
Dana Cronin <cronin43224235@gmail.com>
2019-09-24 22:31:40 -0400

    adding notes for later reference in array modification

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,12 +17,16 @@ var pastGenres = []
 //to capture guess into loop to compare and later array for word or incorrect guesses
 
 
-functionWins(){
-    
-} loop 2
+// functionWins(){ ----arrayName.push() allows to add value to an egsisting array / .unshift adds items to begining of array
+// } loop 2
 
-functionWord(){
-// word chosing from array 
-//var currentWord = genres[Math.floor(Math.random*genres.length)];
-//loop to ensure not already used
-} loop 3
+// functionWord(){
+// // word chosing from array 
+// //var currentWord = genres[Math.floor(Math.random*genres.length)];
+// //loop to ensure not already used
+// } loops 3, 4 and 5?
+//functionDisplay()?
+//functionCompare()?
+
+//.pop() and .shift() remove respectively last and first items from array
+//The splice() method allows us to add/remove items to/from an array, and to specifically indicate the index of the elements that have to be added /removed
